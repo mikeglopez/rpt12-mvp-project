c28f9aeedc06205976d58d69463234616d0f9fc5
Mike <mikeglopez.91@gmail.com>
2019-06-24 13:08:59 -0500

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: "airbnb",
+  rules: {
+    'no-console': 'off',
+    'comma-dangle': 'off',
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'max-len': 'off'
+  }
+};
